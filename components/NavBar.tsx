f1d18b01679d6d76b13b01c33d32cba927896f6b
mishbahh <mishbahussuduri@gmail.com>
2025-06-25 19:13:07 +0700

    finish - slicing home page

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,18 +1,53 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
+import { MdSearch } from "react-icons/md";
 import Button from "./Button";
+import AccountDropdown from "./AccountDropdown";
 
 type Props = {};
 
 const NavBar: React.FC<Props> = () => {
+    const [keyword, setKeyword] = useState("");
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const isLoggedIn = true
+
     return (
         <header className="h-16 border-b border-slate-200 flex items-center justify-between px-24">
             <Link href="/">
                 <img src="/images/logo-with-text.svg" className="h-8"/>
             </Link>
 
-            <Link href="/auth/sign-in">
-                    <Button>Sign In</Button>
-            </Link>
+            <div className="w-[720px] absolute left-1/2 -translate-x-1/2 flex items-center">
+                <MdSearch className="text-slate-400 mr-4" size={24} />
+                <input 
+                    className="font-sans text-sm placeholder-slate-400 text-slate-900 outline-none"
+                    type="text" 
+                    placeholder="Search" 
+                    value={keyword}
+                    onChange={(e) => setKeyword(e.target.value)}
+                />
+            </div>
+
+            {isLoggedIn && (
+                <div className="relative">
+                    <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                        <img 
+                            className="w-10 h-10 rounded-full object-cover"
+                            src="/images/dummy-avatar.png" 
+                            alt="John Doe" 
+                        />
+                    </button>
+                    
+                    {isDropdownOpen && <AccountDropdown />}
+                </div>
+            )}
+            {!isLoggedIn && (
+                <Link href="/auth/sign-in">
+                        <Button>Sign In</Button>
+                </Link>
+            )}  
         </header>
     );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className='font-sans'>{children}</body>
+      <body className="font-sans">{children}</body>
     </html>
   )
 }
